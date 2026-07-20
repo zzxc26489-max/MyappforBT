d786085f32c6b378e1c9810dce2f61d4a6777ce0
zzxc26489-max <zzxc26489@gmail.com>
2026-07-20 11:31:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'crm-romanychev-v5';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg',
+    './icon-192.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('📦 Кэшируем файлы');
+                return cache.addAll(urlsToCache);
+            })
+            .then(()=>self.skipWaiting())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    if (event.request.mode === 'navigate') {
+        event.respondWith(fetch(event.request).then(response => {
+            const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy));return response;
+        }).catch(()=>caches.match('./index.html')));
+        return;
+    }
+    event.respondWith(
+        caches.match(event.request).then(cached => cached || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(()=>self.clients.claim())
+    );
+});
