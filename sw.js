398569bc8c8dae95a61e62459aae147f551baa58
zzxc26489-max <zzxc26489@gmail.com>
2026-07-24 01:51:33 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='crm-bt-v18-5-3';
+const CACHE_NAME='crm-bt-v18-5-4';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
